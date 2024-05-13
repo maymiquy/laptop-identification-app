c93898a5d000e0040cca0e315991c201ba95b1c3
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-05-13 08:44:45 +0700

    fix: redefining routes

--- routes/web.php
+++ routes/web.php
@@ -6,63 +6,43 @@ use App\Http\Controllers\KerusakanController;
 use App\Http\Controllers\AturanController;
 use App\Http\Controllers\OSController;
 use App\Http\Controllers\KonsultasiController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect('/konsultasi');
 });
 
+Route::get('/konsultasi', [KonsultasiController::class, 'start'])->name('konsultasi.index');
+Route::post('/konsultasi', [KonsultasiController::class, 'store'])->name('konsultasi.store');
+Route::get('/konsultasi/pertanyaan/{gejala}', [KonsultasiController::class, 'question'])->name('konsultasi.question');
+Route::post('/konsultasi/pertanyaan', [KonsultasiController::class, 'next'])->name('konsultasi.next');
+
+Route::get('/konsultasi/hasil/{konsultasi}', [KonsultasiController::class, 'result'])->name('konsultasi.result');
+
 Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
+    return view('dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::resource('/dashboard/gejala', GejalaController::class)->except('show')->middleware(['auth', 'verified']);
+
+Route::resource('/dashboard/kerusakan', KerusakanController::class)->except('show')->middleware(['auth', 'verified']);
+
+Route::resource('/dashboard/aturan', AturanController::class)->except('show')->middleware(['auth', 'verified']);
+
+Route::resource('/dashboard/os', OSController::class)->except('show')
+    ->parameters([
+        'os' => 'os'
+    ])->middleware(['auth', 'verified']);
+
+Route::get('/dashboard/konsultasi', [KonsultasiController::class, 'index'])->middleware(['auth', 'verified']);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-    // Route::get('/gejala', [GejalaController::class, 'index'])->name('gejala.index');
-    // Route::get('/gejala/create', [GejalaController::class, 'create'])->name('gejala.create');
-    // Route::post('/gejala', [GejalaController::class, 'store'])->name('gejala.store');
-    // Route::get('/gejala/{gejala}/edit', [GejalaController::class, 'edit'])->name('gejala.edit');
-    // Route::patch('/gejala/{gejala}', [GejalaController::class, 'update'])->name('gejala.update');
-    // Route::delete('/gejala/{gejala}', [GejalaController::class, 'destroy'])->name('gejala.destroy');
-
-    Route::resource('/gejala', GejalaController::class)->except('show')
-        ->names([
-            'index' => 'gejala.index',
-            'create' => 'gejala.create',
-            'store' => 'gejala.store',
-            'edit' => 'gejala.edit'
-        ]);;
-
-
-    Route::resource('/dashboard/kerusakan', KerusakanController::class)->except('show')
-        ->names('kerusakan');
-
-    Route::resource('/dashboard/aturan', AturanController::class)->except('show')
-        ->names('aturan');
-
-    Route::resource('/dashboard/os', OSController::class)->except('show')
-        ->names('os')
-        ->parameters([
-            'os' => 'os'
-        ]);
-
-    Route::get('/konsultasi', [KonsultasiController::class, 'start'])->name('konsultasi.index');
-    Route::post('/konsultasi', [KonsultasiController::class, 'store'])->name('konsultasi.store');
-    Route::get('/konsultasi/pertanyaan/{gejala}', [KonsultasiController::class, 'question'])->name('konsultasi.question');
-    Route::post('/konsultasi/pertanyaan', [KonsultasiController::class, 'next'])->name('konsultasi.next');
-
-    Route::get('/konsultasi/hasil/{konsultasi}', [KonsultasiController::class, 'result'])->name('konsultasi.result');
 });
+
 
 Route::get('/test-aturan', [AturanController::class, 'test']);
 Route::get('/test-konsul', [KonsultasiController::class, 'test']);
