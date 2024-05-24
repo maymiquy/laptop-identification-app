cd380357c72b44656464cea7343dfbfad978cf0b
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2024-05-24 14:44:13 +0700

    chore: improve ui paginate

--- resources/js/Components/Pagination.jsx
+++ resources/js/Components/Pagination.jsx
@@ -3,23 +3,85 @@ import React from "react";
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
     const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
+    const handlePrevPage = () => {
+        if (currentPage > 1) {
+            onPageChange(currentPage - 1);
+        }
+    };
+
+    const handleNextPage = () => {
+        if (currentPage < totalPages) {
+            onPageChange(currentPage + 1);
+        }
+    };
+
     return (
         <nav>
             <ul className="inline-flex items-center -space-x-px">
+                <li>
+                    <button
+                        onClick={handlePrevPage}
+                        className={`px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 ${
+                            currentPage === 1
+                                ? "cursor-not-allowed opacity-50"
+                                : "cursor-pointer"
+                        }`}
+                    >
+                        <span className="sr-only">Previous</span>
+                        <svg
+                            aria-hidden="true"
+                            className="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                                clipRule="evenodd"
+                            ></path>
+                        </svg>
+                    </button>
+                </li>
                 {pageNumbers.map((pageNumber) => (
                     <li key={pageNumber}>
                         <button
                             onClick={() => onPageChange(pageNumber)}
-                            className={`px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 ${
+                            className={`border border-gray-300  ${
                                 currentPage === pageNumber
-                                    ? "z-10 text-blue-600 bg-blue-50 border-blue-300 hover:bg-blue-200"
-                                    : ""
+                                    ? "px-4 py-[10px] leading-tight font-bold text-zinc-100 bg-indigo-500 hover:bg-indigo-600 hover:text-white rounded"
+                                    : "px-4 py-2 leading-tight font-medium text-gray-700 bg-white hover:bg-gray-100 hover:text-gray-900 "
                             }`}
                         >
                             {pageNumber}
                         </button>
                     </li>
                 ))}
+                <li>
+                    <button
+                        onClick={handleNextPage}
+                        className={`px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 ${
+                            currentPage === totalPages
+                                ? "cursor-not-allowed opacity-50"
+                                : "cursor-pointer"
+                        }`}
+                    >
+                        <span className="sr-only">Next</span>
+                        <svg
+                            aria-hidden="true"
+                            className="w-5 h-5"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fillRule="evenodd"
+                                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                clipRule="evenodd"
+                            ></path>
+                        </svg>
+                    </button>
+                </li>
             </ul>
         </nav>
     );
